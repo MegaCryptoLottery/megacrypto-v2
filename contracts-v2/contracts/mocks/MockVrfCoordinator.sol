d0456a58e54e6cb8b74ae2cf5b0c69746610bf13
MegaCryptoLottery <clebermonteiro1@hotmail.com>
2026-09-16 18:54:56 -0400

    Add isolated hardened lottery contract workspace

---
+++ contracts-v2/contracts/mocks/MockVrfCoordinator.sol
@@ -0,0 +1,14 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {IVRFV2PlusCoordinator} from "../Interfaces.sol";
+
+interface IConsumer { function fulfillRandomWords(uint256 requestId, uint256[] calldata words) external; }
+
+contract MockVrfCoordinator is IVRFV2PlusCoordinator {
+    uint256 public nextRequestId = 1;
+    mapping(uint256 => address) public consumerForRequest;
+    function requestRandomWords(RandomWordsRequest calldata) external returns (uint256 requestId) { requestId = nextRequestId++; consumerForRequest[requestId] = msg.sender; }
+    function fulfill(uint256 requestId, uint256 word) external { uint256[] memory words = new uint256[](1); words[0] = word; IConsumer(consumerForRequest[requestId]).fulfillRandomWords(requestId, words); }
+}
+
