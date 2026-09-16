531725264bc68010ff3b197b7db659ecb06fe5d2
MegaCryptoLottery <clebermonteiro1@hotmail.com>
2026-09-16 19:22:11 -0400

    Complete hardened contract pre-testnet gate

--- contracts-v2/contracts/mocks/MockVrfCoordinator.sol
+++ contracts-v2/contracts/mocks/MockVrfCoordinator.sol
@@ -8,7 +8,8 @@ interface IConsumer { function fulfillRandomWords(uint256 requestId, uint256[] c
 contract MockVrfCoordinator is IVRFV2PlusCoordinator {
     uint256 public nextRequestId = 1;
     mapping(uint256 => address) public consumerForRequest;
-    function requestRandomWords(RandomWordsRequest calldata) external returns (uint256 requestId) { requestId = nextRequestId++; consumerForRequest[requestId] = msg.sender; }
+    bytes public lastExtraArgs;
+    function requestRandomWords(RandomWordsRequest calldata request) external returns (uint256 requestId) { requestId = nextRequestId++; consumerForRequest[requestId] = msg.sender; lastExtraArgs = request.extraArgs; }
     function fulfill(uint256 requestId, uint256 word) external { uint256[] memory words = new uint256[](1); words[0] = word; IConsumer(consumerForRequest[requestId]).fulfillRandomWords(requestId, words); }
 }
 
