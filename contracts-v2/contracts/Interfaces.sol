e1a28b96f6363ae8e6c26e896d09429c8e8ddc88
MegaCryptoLottery <clebermonteiro1@hotmail.com>
2026-09-16 18:54:44 -0400

    Add isolated hardened lottery contract workspace

---
+++ contracts-v2/contracts/Interfaces.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+interface IERC20Like {
+    function transfer(address to, uint256 value) external returns (bool);
+    function transferFrom(address from, address to, uint256 value) external returns (bool);
+    function balanceOf(address account) external view returns (uint256);
+    function decimals() external view returns (uint8);
+}
+
+library SafeERC20 {
+    function safeTransfer(IERC20Like token, address to, uint256 value) internal {
+        (bool ok, bytes memory data) = address(token).call(abi.encodeWithSelector(token.transfer.selector, to, value));
+        require(ok && (data.length == 0 || abi.decode(data, (bool))), "SAFE_ERC20_TRANSFER");
+    }
+    function safeTransferFrom(IERC20Like token, address from, address to, uint256 value) internal {
+        (bool ok, bytes memory data) = address(token).call(abi.encodeWithSelector(token.transferFrom.selector, from, to, value));
+        require(ok && (data.length == 0 || abi.decode(data, (bool))), "SAFE_ERC20_TRANSFER_FROM");
+    }
+}
+
+interface IVRFV2PlusCoordinator {
+    struct RandomWordsRequest {
+        bytes32 keyHash;
+        uint256 subId;
+        uint16 requestConfirmations;
+        uint32 callbackGasLimit;
+        uint32 numWords;
+        bytes extraArgs;
+    }
+    function requestRandomWords(RandomWordsRequest calldata request) external returns (uint256 requestId);
+}
+
+interface IAutomationCompatible {
+    function checkUpkeep(bytes calldata checkData) external view returns (bool upkeepNeeded, bytes memory performData);
+    function performUpkeep(bytes calldata performData) external;
+}
+
+interface IMigrationReceiver {
+    function migrationReceiverMagic(address token, uint256 chainId) external view returns (bytes4);
+    function receiveMigration(address token, uint256 amount) external;
+}
+
