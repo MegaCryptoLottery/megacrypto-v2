077ddfc228355a5d50ac1cb5484142bbf0070ea4
MegaCryptoLottery <clebermonteiro1@hotmail.com>
2026-09-16 18:54:59 -0400

    Add isolated hardened lottery contract workspace

---
+++ contracts-v2/contracts/mocks/MockMigrationReceiver.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: MIT
+pragma solidity 0.8.28;
+
+import {IMigrationReceiver, IERC20Like} from "../Interfaces.sol";
+
+contract MockMigrationReceiver is IMigrationReceiver {
+    bytes4 public constant MAGIC = bytes4(keccak256("MegaCryptoLotteryMigrationReceiverV2"));
+    address public receivedToken;
+    uint256 public receivedAmount;
+    function migrationReceiverMagic(address, uint256) external pure returns (bytes4) { return MAGIC; }
+    function receiveMigration(address token, uint256 amount) external { require(IERC20Like(token).balanceOf(address(this)) >= amount, "NOT_RECEIVED"); receivedToken = token; receivedAmount += amount; }
+}
+
+contract InvalidMigrationReceiver { }
+
